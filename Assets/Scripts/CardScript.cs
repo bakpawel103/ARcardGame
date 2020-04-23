fffcc55c3c47b877de4763041fbf27f98420fe80
bakpawel <bakpawel103@gmail.com>
2020-04-23 15:06:04 +0200

    Added Unity Project to repo

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                Debug.Log("Tapped");
+                if (raycastHit.collider.CompareTag("Card"))
+                {
+                    Debug.Log("Tapped Card");
+                    if (!HandManager.handShown)
+                    {
+                        Debug.Log("Showing");
+                        HandManager.instance.ShowHand();
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("Tapped World");
+                if (HandManager.handShown)
+                {
+                    Debug.Log("Hiding");
+                    HandManager.instance.HideHand();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HandManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour
+{
+    public double cardDistance = 0.3f;
+    public float closer = 0.1f;
+    public static bool handShown = true;
+
+    public static HandManager instance = null;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+     
+        instance = this;
+        DontDestroyOnLoad( gameObject );
+    }
+    void Start()
+    {
+        var halfCount = 0.0;
+        var startingXPos = 0.0;
+        var startingZPos = 0.0f;
+        if (transform.childCount % 2 != 0)
+        {
+            halfCount = Math.Floor((float) transform.childCount / 2);
+            startingXPos = -halfCount * cardDistance;
+        }
+        else
+        {
+            halfCount = Math.Floor((float) transform.childCount / 2);
+            startingXPos = (-halfCount * cardDistance) - cardDistance/2;
+        }
+        for (var i = 0; i < transform.childCount; ++i, startingXPos += cardDistance, startingZPos += closer)
+        {
+            transform.GetChild(i).transform.position = new Vector3((float) startingXPos, transform.GetChild(i).transform.position.y, transform.GetChild(i).transform.position.z - startingZPos);
+        }
+    }
+
+    public void HideHand()
+    {
+        transform.position -= transform.forward * 0.9f;
+        handShown = false;
+    }
+    
+    public void ShowHand()
+    {
+        transform.position += transform.forward * 0.9f;
+        handShown = true;
+    }
+}
