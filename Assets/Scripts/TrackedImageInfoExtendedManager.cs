ecb15eafecb65e7dbe410157415292c9a9a0f808
bakpawel <bakpawel103@gmail.com>
2020-04-23 20:07:12 +0200

    Added Image Tracking

---
+++ Assets/Scripts/TrackedImageInfoExtendedManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class TrackedImageInfoExtendedManager : MonoBehaviour
+{
+    private ARTrackedImageManager m_TrackedImageManager;
+
+    private void Awake()
+    {
+        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    private void OnDisable()
+    {
+        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            trackedImage.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        }
+    }
+}
