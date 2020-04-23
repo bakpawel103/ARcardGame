b9ac65ca8dcaec53719ee0981101d18b9e596c19
bakpawel <bakpawel103@gmail.com>
2020-04-23 21:11:57 +0200

    Added card moving to camera on click on card stack

---
+++ Assets/Scripts/CardsStackManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class CardsStackManager : MonoBehaviour
+{
+    private GameObject card;
+    private float speed;
+    private GameObject camera;
+    private bool moveCardToHand;
+
+    private void Awake()
+    {
+        camera = GameObject.FindWithTag("Camera");
+        card = GameObject.FindWithTag("Card");
+        moveCardToHand = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!moveCardToHand)
+        {
+            moveCardToHand = !moveCardToHand;
+            GameManager.instance.SetCardVisibility(moveCardToHand);
+        }
+    }
+
+    void Update () {
+        if(moveCardToHand) {
+            float step = speed * Time.deltaTime;
+            card.transform.position = Vector2.MoveTowards(card.transform.position, camera.transform.position, step);
+        }
+    }
+}
