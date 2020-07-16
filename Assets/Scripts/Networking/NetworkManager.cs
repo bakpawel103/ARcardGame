bb66d717e1371e4f9e5c01cb5c40c0904ddccd1a
bakpawel <bakpawel103@gmail.com>
2020-07-17 00:21:59 +0200

    Changed Lobby Scene from GUI to UI

--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -2,35 +2,32 @@
 using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
     public int maxRoomPlayer = 4;
-    
-     //Our player name
-    string playerName = "Player 1";
-    //Users are separated from each other by gameversion (which allows you to make breaking changes).
-    string gameVersion = "0.9";
-    //The list of created rooms
-    List<RoomInfo> createdRooms = new List<RoomInfo>();
-    //Use this name when creating a Room
-    string roomName = "Room 1";
-    Vector2 roomListScroll = Vector2.zero;
-    bool joiningRoom = false;
+    public GameObject scrollViewItemItemPrefab;
 
-    private GUIStyle buttonsStyle;
-    private GUIStyle labelStyle;
-    private GUIStyle textFieldStyle;
-    private GUIStyle smallLabelStyle;
-    private GUIStyle smallButtonsStyle;
-    private GUIStyle windowStyle;
+    private GameObject playerName;
+    private string gameVersion = "0.9";
+    private List<RoomInfo> createdRooms = new List<RoomInfo>();
+    private GameObject roomName;
+    private GameObject scrollViewContentGO;
+    private bool joiningRoom = false;
+
+    private GUIStyle lobbyGUISkin;
 
     private int spaceBetweenGUI = 20;
 
-    // Use this for initialization
     void Start()
     {
+        playerName = GameObject.FindGameObjectWithTag("PlayerNameInput");
+        roomName = GameObject.FindGameObjectWithTag("RoomNameInput");
+        scrollViewContentGO = GameObject.FindGameObjectWithTag("ScrollViewContent");
+
         //This makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
         PhotonNetwork.AutomaticallySyncScene = true;
 
@@ -41,11 +38,84 @@ public class NetworkManager : MonoBehaviourPunCallbacks
             // Connect to the photon master-server. We use the settings saved in PhotonServerSettings (a .asset file in this project)
             PhotonNetwork.ConnectUsingSettings();
         }
+
+        GenerateScrollView();
+    }
+
+    void Update()
+    {
+    }
+
+    public void CreateJoinRoom()
+    {
+        Debug.Log(roomName);
+        if (roomName.GetComponent<InputField>().text != "")
+        {
+            joiningRoom = true;
+
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.IsOpen = true;
+            roomOptions.IsVisible = true;
+            roomOptions.MaxPlayers = (byte) maxRoomPlayer;
+
+            PhotonNetwork.JoinOrCreateRoom(roomName.GetComponent<InputField>().text, roomOptions, TypedLobby.Default);
+        }
+    }
+
+    public void RefreshRooms()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            //Re-join Lobby to get the latest Room list
+            PhotonNetwork.JoinLobby(TypedLobby.Default);
+        }
+        else
+        {
+            //We are not connected, estabilish a new connection
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        GenerateScrollView();
+    }
+
+    private void GenerateScrollView()
+    {
+        DeleteAllChildrenFromGameObject(scrollViewContentGO);
+
+        for (int i = 0; i < createdRooms.Count; i++)
+        {
+            GameObject itemPrefabInstance = Instantiate(scrollViewItemItemPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            if (itemPrefabInstance != null)
+            {
+                itemPrefabInstance.transform.GetChild(0).GetComponent<Text>().text = createdRooms[i].Name;
+                itemPrefabInstance.transform.GetChild(1).GetComponent<Text>().text =
+                    createdRooms[i].PlayerCount + "/" + createdRooms[i].MaxPlayers;
+
+                var i1 = i;
+                itemPrefabInstance.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    Debug.Log(i1);
+                    joiningRoom = true;
+                    PhotonNetwork.NickName = playerName.GetComponent<InputField>().text;
+                    PhotonNetwork.JoinRoom(createdRooms[i1].Name);
+                });
+                itemPrefabInstance.transform.SetParent(scrollViewContentGO.transform, false);
+            }
+        }
+    }
+
+    private void DeleteAllChildrenFromGameObject(GameObject gameObject)
+    {
+        if (gameObject.transform.GetChildCount() > 0)
+        {
+            Destroy(gameObject.transform.GetChild(0).gameObject);
+        }
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        Debug.Log("OnFailedToConnectToPhoton. StatusCode: " + cause.ToString() + " ServerAddress: " + PhotonNetwork.ServerAddress);
+        Debug.Log("OnFailedToConnectToPhoton. StatusCode: " + cause.ToString() + " ServerAddress: " +
+                  PhotonNetwork.ServerAddress);
     }
 
     public override void OnConnectedToMaster()
@@ -60,141 +130,13 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         Debug.Log("We have received the Room list");
         //After this callback, update the room list
         createdRooms = roomList;
-    }
-
-    void OnGUI()
-    {
-        buttonsStyle = new GUIStyle(GUI.skin.button);
-        buttonsStyle.fontSize = 45;
-        smallButtonsStyle = new GUIStyle(GUI.skin.button);
-        smallButtonsStyle.fontSize = 40;
-        labelStyle = new GUIStyle(GUI.skin.label);
-        labelStyle.fontSize = 45;
-        smallLabelStyle = new GUIStyle(GUI.skin.label);
-        smallLabelStyle.fontSize = 40;
-        textFieldStyle = new GUIStyle(GUI.skin.textField);
-        textFieldStyle.fontSize = 45;
-        windowStyle = new GUIStyle (GUI.skin.window); 
-        // do whatever you want with this style, e.g.:
-        windowStyle.padding = new RectOffset(25,25,25,25);
-        
-        GUI.Window(0, new Rect(10, Screen.height/2-Screen.height/3/2, Screen.width-20, Screen.height/2), LobbyWindow, "", windowStyle);
-    }
-
-    void LobbyWindow(int index)
-    {
-        GUILayout.BeginVertical();
-            GUILayout.BeginHorizontal();
-                GUILayout.FlexibleSpace();
-                //Set player name and Refresh Room button
-                GUILayout.Label("Player Name: ", labelStyle, GUILayout.Width(300));
-                //Player name text field
-                playerName = GUILayout.TextField(playerName, textFieldStyle, GUILayout.Width(300));
-                
-                GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-        GUILayout.EndVertical();
-
-        GUILayout.Space(spaceBetweenGUI);
-        
-        GUILayout.BeginVertical();
-            GUILayout.BeginHorizontal();
-                GUILayout.FlexibleSpace();
-                //Room name text field
-                roomName = GUILayout.TextField(roomName, textFieldStyle,GUILayout.Width(400));
-
-                if (GUILayout.Button("Create Room", buttonsStyle,GUILayout.Width(325)))
-                {
-                    if (roomName != "")
-                    {
-                        joiningRoom = true;
-
-                        RoomOptions roomOptions = new RoomOptions();
-                        roomOptions.IsOpen = true;
-                        roomOptions.IsVisible = true;
-                        roomOptions.MaxPlayers = (byte)maxRoomPlayer;
-
-                        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
-                    }
-                }
-                GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-        GUILayout.EndVertical();
-
-        GUILayout.Space(spaceBetweenGUI);
-
-        //Scroll through available rooms
-        roomListScroll = GUILayout.BeginScrollView(roomListScroll,false, true);
-            if (createdRooms.Count == 0)
-            {
-                GUILayout.Label("No Rooms were created yet...", smallLabelStyle);
-            }
-            else
-            {
-                for (int i = 0; i < createdRooms.Count; i++)
-                {
-                    GUILayout.BeginHorizontal("box");
-                        GUILayout.Label(createdRooms[i].Name, labelStyle, GUILayout.Width(400));
-                        GUILayout.Label(createdRooms[i].PlayerCount + "/" + createdRooms[i].MaxPlayers, labelStyle);
-
-                        GUILayout.FlexibleSpace();
-
-                        if (GUILayout.Button("Join Room", smallButtonsStyle))
-                        {
-                            joiningRoom = true;
-
-                            //Set our Player name
-                            PhotonNetwork.NickName = playerName;
-
-                            //Join the Room
-                            PhotonNetwork.JoinRoom(createdRooms[i].Name);
-                        }
-                    GUILayout.EndHorizontal();
-                }
-            }
-        GUILayout.EndScrollView();
-        
-        GUILayout.Space(spaceBetweenGUI);
-        
-        GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            GUI.enabled = (PhotonNetwork.NetworkClientState == ClientState.JoinedLobby || PhotonNetwork.NetworkClientState == ClientState.Disconnected) && !joiningRoom;
-            if (GUILayout.Button("Refresh", buttonsStyle, GUILayout.Width(200)))
-            {
-                if (PhotonNetwork.IsConnected)
-                {
-                    //Re-join Lobby to get the latest Room list
-                    PhotonNetwork.JoinLobby(TypedLobby.Default);
-                }
-                else
-                {
-                    //We are not connected, estabilish a new connection
-                    PhotonNetwork.ConnectUsingSettings();
-                }
-            }
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(spaceBetweenGUI);
-
-        GUILayout.BeginHorizontal();
-            GUILayout.Label("Status: " + Regex.Replace(PhotonNetwork.NetworkClientState.ToString(), "([A-Z])", " $1").Trim(), labelStyle);
-
-            if (joiningRoom || !PhotonNetwork.IsConnected || PhotonNetwork.NetworkClientState != ClientState.JoinedLobby)
-            {
-                GUI.enabled = false;
-            }
-        GUILayout.EndHorizontal();
-
-        if (joiningRoom)
-        {
-            GUI.enabled = true;
-            GUI.Label(new Rect(Screen.width/2, Screen.height/2, Screen.width, 40), "Connecting...", labelStyle);
-        }
+        GenerateScrollView();
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
-        Debug.Log("OnCreateRoomFailed got called. This can happen if the room exists (even if not visible). Try another room name.");
+        Debug.Log(
+            "OnCreateRoomFailed got called. This can happen if the room exists (even if not visible). Try another room name.");
         joiningRoom = false;
     }
 
@@ -214,7 +156,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         Debug.Log("OnCreatedRoom");
         //Set our player name
-        PhotonNetwork.NickName = playerName;
+        PhotonNetwork.NickName = playerName.GetComponent<InputField>().text;
         //Load the Scene called Game (Make sure it's added to build settings)
         PhotonNetwork.LoadLevel("Game");
     }
