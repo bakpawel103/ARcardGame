9a947600b267a327a21b93c15c9b0375e3751216
bakpawel <bakpawel103@gmail.com>
2020-11-18 17:33:52 +0100

    Deleting whole unity project. Will be created new one that will use Microsoft Mixed Reality Toolkit + AR Foundation

--- Assets/Scripts/GameMechanisms/Cards/Attributes.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Xml.Serialization;
-
-[XmlRoot("Attributes")]
-public class Attributes
-{
-    [XmlElement("Attack")] public int attack { get; set; }
-    [XmlElement("Defence")] public int defence { get; set; }
-}
--- Assets/Scripts/GameMechanisms/Cards/ICard.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface ICard
-{
-    int ID { get; }
-}
